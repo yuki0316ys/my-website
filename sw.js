017f81494edc84078c24475f5673a20994f6c3c9
yuki0316ys <kd7hpr5qfk@privaterelay.appleid.com>
2026-05-26 20:51:11 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,3 +5,12 @@ self.addEventListener("install", event => {
 self.addEventListener("fetch", event => {
 
 });
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => caches.delete(key))
+      );
+    })
+  );
+});
