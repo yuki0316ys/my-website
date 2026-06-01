5d2d39af37fe4a993637b3358a6673889975c060
yuki0316ys <kd7hpr5qfk@privaterelay.appleid.com>
2026-06-01 12:11:09 +0900

    Bump style cache for preview heading font

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const FANCLUB_URL = "https://www.youtube.com/channel/UCdZMz61Y8oS4kfC9CP4esTA/join";
-const STYLE_VERSION = "2026060103";
+const STYLE_VERSION = "2026060104";
 
 self.addEventListener("install", event => {
   console.log("Service Worker Installed");
