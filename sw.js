46951311636b20ca4be54d884d4e5b42e0e1272f
yuki0316ys <kd7hpr5qfk@privaterelay.appleid.com>
2026-06-01 09:47:51 +0900

    Bump style cache for store cleanup

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const FANCLUB_URL = "https://www.youtube.com/channel/UCdZMz61Y8oS4kfC9CP4esTA/join";
-const STYLE_VERSION = "2026060102";
+const STYLE_VERSION = "2026060103";
 
 self.addEventListener("install", event => {
   console.log("Service Worker Installed");
